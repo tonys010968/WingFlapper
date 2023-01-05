af8fa82b7e96e26aa1e2676cd262e14823650dfa
Jacyn <anthony@thesavocas.com>
2023-01-05 14:12:49 -0500

    Finished MVP1 of Game

--- Wing Flapper/Assets/BirdScript.cs
+++ Wing Flapper/Assets/BirdScript.cs
@@ -7,6 +7,7 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D myRigidbody;
     public float flapStrength;
     public LogicScript logic;
+    public bool birdIsAlive = true;
 
 
     // Start is called before the first frame update
@@ -18,7 +19,7 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
@@ -28,6 +29,7 @@ public class BirdScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         logic.gameOver();
+        birdIsAlive = false;
     }
 
 }
