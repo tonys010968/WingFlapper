dfe3e6b000efa4562e2c864a4e6066187a2619f7
Jacyn <anthony@thesavocas.com>
2023-01-05 14:06:45 -0500

    Added GameOver

--- Wing Flapper/Assets/BirdScript.cs
+++ Wing Flapper/Assets/BirdScript.cs
@@ -6,12 +6,13 @@ public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
@@ -23,4 +24,10 @@ public class BirdScript : MonoBehaviour
         }
 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+    }
+
 }
--- Wing Flapper/Assets/LogicScript.cs
+++ Wing Flapper/Assets/LogicScript.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -15,4 +17,13 @@ public class LogicScript : MonoBehaviour
         scoreText.text = playerScore.ToString();
     }
 
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
 }
